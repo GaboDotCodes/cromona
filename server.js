50a62d896db6ea98ef3c5d03e067f9220a8d524c
GaboDotCodes <gabedu2000@gmail.com>
2020-06-25 06:28:25 -0500

    Add Sentry service to handle errors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const app = express();
+const sentry = require('@sentry/node');
 
-const { PORT } = process.env;
+const { PORT, DSN_SENTRY, ENV_SENTRY } = process.env;
 const { log } = console;
 
+sentry.init({ dsn: `${DSN_SENTRY}`, environment: `${ENV_SENTRY}`});
+const app = express();
 
 app.listen(PORT, () => {
     log(`Server on http://localhost:${PORT}`)
