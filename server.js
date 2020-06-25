144f2982fa018dddfb78f46a738ab9d46cab931e
GaboDotCodes <gabedu2000@gmail.com>
2020-06-25 14:51:52 -0500

    Applying Eslint Airbnb Style

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const sentry = require('@sentry/node');
 const { PORT, DSN_SENTRY, ENV_SENTRY } = process.env;
 const { log } = console;
 
-sentry.init({ dsn: `${DSN_SENTRY}`, environment: `${ENV_SENTRY}`});
+sentry.init({ dsn: `${DSN_SENTRY}`, environment: `${ENV_SENTRY}` });
 const app = express();
 
 app.listen(PORT, () => {
-    log(`Server on http://localhost:${PORT}`)
-})
+  log(`Server on http://localhost:${PORT}`);
+});
