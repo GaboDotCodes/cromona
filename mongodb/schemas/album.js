e8ca8fea7358f04ca457484eef79428a23671b52
GaboDotCodes <gabedu2000@gmail.com>
2020-06-26 10:44:33 -0500

    Album schema completed

--- mongodb/schemas/album.js
+++ mongodb/schemas/album.js
@@ -2,6 +2,17 @@ const { Schema, model } = require('mongoose');
 
 const { ObjectId } = Schema.Types;
 
-const albumSchema = new Schema({});
+const albumSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  editions: [
+    {
+      type: ObjectId,
+      ref: 'Edition',
+    },
+  ],
+});
 
 module.exports = model('Album', albumSchema);
