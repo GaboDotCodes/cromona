e2efeda2aea57ab7876d8d5511bf245aef1022b6
GaboDotCodes <gabedu2000@gmail.com>
2020-06-26 21:19:41 -0500

    Editorial schema finished

--- mongodb/schemas/editorial.js
+++ mongodb/schemas/editorial.js
@@ -1,7 +1,24 @@
 const { Schema, model } = require('mongoose');
+const { isURL } = require('validator');
 
 const { ObjectId } = Schema.Types;
 
-const editorialSchema = new Schema({});
+const editorialSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  logo: {
+    type: String,
+    required: true,
+    validate: isURL,
+  },
+  albums: [
+    {
+      type: ObjectId,
+      ref: 'Edition',
+    },
+  ],
+});
 
 module.exports = model('Editorial', editorialSchema);
