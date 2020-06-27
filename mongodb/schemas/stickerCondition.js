8442ae0d226f0cec7a60b9020a8c2dc8b98d7581
GaboDotCodes <gabedu2000@gmail.com>
2020-06-26 22:42:16 -0500

    Remove useless schema

--- mongodb/schemas/stickerCondition.js
+++
@@ -1,7 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const { ObjectId } = Schema.Types;
-
-const stickerConditionSchema = new Schema({});
-
-module.exports = model('StickerCondition', stickerConditionSchema);
