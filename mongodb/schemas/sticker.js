61f3b318ff9185b81d736f0b31a5b5f87771cb06
GaboDotCodes <gabedu2000@gmail.com>
2020-06-26 22:08:54 -0500

    Sticker schema finished

--- mongodb/schemas/sticker.js
+++ mongodb/schemas/sticker.js
@@ -1,7 +1,49 @@
 const { Schema, model } = require('mongoose');
+const { isURL } = require('validator');
 
 const { ObjectId } = Schema.Types;
 
-const stickerSchema = new mongoose.Schema({});
+const stickerSchema = new Schema({
+  album: {
+    type: ObjectId,
+    ref: 'Edition',
+  },
+  number: {
+    type: Number,
+    require: true,
+    unique: true,
+  },
+  tittle: {
+    type: String,
+    require: true,
+  },
+  image: {
+    type: String,
+    require: true,
+    validate: isURL,
+  },
+  compatibleWith: [
+    {
+      type: ObjectId,
+      ref: 'Sticker',
+    },
+  ],
+  categories: [
+    {
+      title: {
+        type: String,
+      },
+      optional: {
+        type: Boolean,
+      },
+      options: [
+        {
+          type: ObjectId,
+          ref: 'Category',
+        },
+      ],
+    },
+  ],
+});
 
 module.exports = model('Sticker', stickerSchema);
