44f07a28839736e0397fdc3329d412fc49124114
GaboDotCodes <gabedu2000@gmail.com>
2020-06-30 17:41:28 -0500

    Add getAllAlbums Query

--- graphql/resolvers/album.resolvers.js
+++ graphql/resolvers/album.resolvers.js
@@ -1,5 +1,6 @@
 const { error } = console;
 const addAlbum = require('./functions/addAlbum');
+const getAllAlbums = require('./functions/getAllAlbums');
 
 module.exports = {
   Mutation: {
@@ -7,6 +8,17 @@ module.exports = {
       try {
         const albumReturn = await addAlbum(album);
         return albumReturn;
+      } catch (e) {
+        error(e);
+        return e;
+      }
+    },
+  },
+  Query: {
+    getAllAlbums: async () => {
+      try {
+        const albumsReturn = await getAllAlbums();
+        return albumsReturn;
       } catch (e) {
         error(e);
         return e;
