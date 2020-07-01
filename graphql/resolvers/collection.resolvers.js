3fb5f5c56816ffebdc62838f8df317fdb9e8d455
GaboDotCodes <gabedu2000@gmail.com>
2020-07-01 18:35:48 -0500

    Album subfield Collection Schema

--- graphql/resolvers/collection.resolvers.js
+++ graphql/resolvers/collection.resolvers.js
@@ -1,6 +1,7 @@
 const { error } = console;
 const addCollection = require('./functions/addCollection');
 const addCollectionToUser = require('./functions/addCollectionToUser');
+const getAlbumById = require('./functions/getAlbumById');
 
 module.exports = {
   Mutation: {
@@ -9,6 +10,17 @@ module.exports = {
         const collectionReturn = await addCollection(collection);
         await addCollectionToUser(collectionReturn.user, collectionReturn.id);
         return collectionReturn;
+      } catch (e) {
+        error(e);
+        return e;
+      }
+    },
+  },
+  Collection: {
+    async album({ album }) {
+      try {
+        const albumReturn = await getAlbumById(album);
+        return albumReturn;
       } catch (e) {
         error(e);
         return e;
