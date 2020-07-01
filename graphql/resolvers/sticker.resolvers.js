20b447344f09dec87faf2dab60d4886daaca3d30
GaboDotCodes <gabedu2000@gmail.com>
2020-07-01 16:02:20 -0500

    Sticker's album field solved

--- graphql/resolvers/sticker.resolvers.js
+++ graphql/resolvers/sticker.resolvers.js
@@ -1,6 +1,7 @@
 const { error } = console;
 const addSticker = require('./functions/addSticker');
 const addStickerToAlbum = require('./functions/addStickerToAlbum');
+const getAlbumById = require('./functions/getAlbumById');
 
 module.exports = {
   Mutation: {
@@ -9,6 +10,17 @@ module.exports = {
         const stickerReturn = await addSticker(sticker);
         await addStickerToAlbum(stickerReturn.album, stickerReturn.id);
         return stickerReturn;
+      } catch (e) {
+        error(e);
+        return e;
+      }
+    },
+  },
+  Sticker: {
+    async album({ album }) {
+      try {
+        const albumReturn = await getAlbumById(album);
+        return albumReturn;
       } catch (e) {
         error(e);
         return e;
