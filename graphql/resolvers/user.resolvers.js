b6505e8d117bec687b1d16306a40344bb255615c
GaboDotCodes <gabedu2000@gmail.com>
2020-07-02 09:15:36 -0500

    collections ratings and swaps User Resolvers

--- graphql/resolvers/user.resolvers.js
+++ graphql/resolvers/user.resolvers.js
@@ -1,6 +1,9 @@
 const { error } = console;
 const addUser = require('./functions/addUser');
 const getUserById = require('./functions/getUserById');
+const getCollectionsByUserId = require('./functions/getCollectionsByUserId');
+const getRatingsByUserId = require('./functions/getRatingsByUserId');
+const getSwapsByUserId = require('./functions/getSwapsByUserId');
 
 module.exports = {
   Mutation: {
@@ -19,6 +22,35 @@ module.exports = {
       try {
         const userReturn = await getUserById(user);
         return userReturn;
+      } catch (e) {
+        error(e);
+        return e;
+      }
+    },
+  },
+  User: {
+    async collections({ id }) {
+      try {
+        const collectionsReturn = await getCollectionsByUserId(id);
+        return collectionsReturn;
+      } catch (e) {
+        error(e);
+        return e;
+      }
+    },
+    async ratings({ id }) {
+      try {
+        const RatingsReturn = await getRatingsByUserId(id);
+        return RatingsReturn;
+      } catch (e) {
+        error(e);
+        return e;
+      }
+    },
+    async swaps({ id }) {
+      try {
+        const SwapsReturn = await getSwapsByUserId(id);
+        return SwapsReturn;
       } catch (e) {
         error(e);
         return e;
