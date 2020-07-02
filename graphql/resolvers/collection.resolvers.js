20990904900d44555ce93d61cc77f909aa4825ae
GaboDotCodes <gabedu2000@gmail.com>
2020-07-01 21:02:32 -0500

    stickersCollection Resolver

--- graphql/resolvers/collection.resolvers.js
+++ graphql/resolvers/collection.resolvers.js
@@ -2,6 +2,7 @@ const { error } = console;
 const addCollection = require('./functions/addCollection');
 const addCollectionToUser = require('./functions/addCollectionToUser');
 const getAlbumById = require('./functions/getAlbumById');
+const getCollectionWithStickers = require('./functions/getCollectionWithStickers');
 
 module.exports = {
   Mutation: {
@@ -21,6 +22,15 @@ module.exports = {
       try {
         const albumReturn = await getAlbumById(album);
         return albumReturn;
+      } catch (e) {
+        error(e);
+        return e;
+      }
+    },
+    async stickersCollection({ id }) {
+      try {
+        const stickersCollectionReturn = await getCollectionWithStickers(id);
+        return stickersCollectionReturn;
       } catch (e) {
         error(e);
         return e;
