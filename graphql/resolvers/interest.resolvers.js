6844d502bd9044b5f79f10915309057f6dbcc9de
GaboDotCodes <gabedu2000@gmail.com>
2020-07-02 10:28:29 -0500

    Interest's by,to resolvers fields

--- graphql/resolvers/interest.resolvers.js
+++ graphql/resolvers/interest.resolvers.js
@@ -1,5 +1,6 @@
 const { error } = console;
 const addInterest = require('./functions/addInterest');
+const getUserById = require('./functions/getUserById');
 
 module.exports = {
   Mutation: {
@@ -7,6 +8,26 @@ module.exports = {
       try {
         const interestReturn = await addInterest(interest);
         return interestReturn;
+      } catch (e) {
+        error(e);
+        return e;
+      }
+    },
+  },
+  Interest: {
+    async by({ by }) {
+      try {
+        const userReturn = await getUserById(by);
+        return userReturn;
+      } catch (e) {
+        error(e);
+        return e;
+      }
+    },
+    async to({ to }) {
+      try {
+        const userReturn = await getUserById(to);
+        return userReturn;
       } catch (e) {
         error(e);
         return e;
