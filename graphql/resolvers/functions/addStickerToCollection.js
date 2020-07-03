c58218088b04197abed15fca990a811be2daff42
GaboDotCodes <gabedu2000@gmail.com>
2020-07-02 23:10:19 -0500

    Modify addStickerToCollection Function to Reutilize

--- graphql/resolvers/functions/addStickerToCollection.js
+++ graphql/resolvers/functions/addStickerToCollection.js
@@ -1,10 +1,10 @@
 const Collection = require('../../../mongodb/schemas/Collection');
 
-const addStickerToCollection = async (operationDetail) => {
+const addStickerToCollection = async (operationDetail, operation = 1) => {
   const { sticker, collection } = operationDetail;
   const collectionUpdated = await Collection.findOneAndUpdate(
     { _id: collection, 'stickersCollection.sticker': sticker },
-    { $inc: { 'stickersCollection.$.amount': 1 } },
+    { $inc: { 'stickersCollection.$.amount': operation } },
     { new: true, useFindAndModify: true }
   ).populate('stickersCollection.sticker');
   const { stickersCollection } = collectionUpdated;
