96551332eb19d4d2d3776342dcceaa5986e75c87
GaboDotCodes <gabedu2000@gmail.com>
2020-07-02 22:57:32 -0500

    addStickerToCollection Resolver GraphQL

--- graphql/resolvers/collection.resolvers.js
+++ graphql/resolvers/collection.resolvers.js
@@ -3,6 +3,7 @@ const addCollection = require('./functions/addCollection');
 const addCollectionToUser = require('./functions/addCollectionToUser');
 const getAlbumById = require('./functions/getAlbumById');
 const getCollectionWithStickers = require('./functions/getCollectionWithStickers');
+const addStickerToCollection = require('./functions/addStickerToCollection');
 
 module.exports = {
   Mutation: {
@@ -11,6 +12,15 @@ module.exports = {
         const collectionReturn = await addCollection(collection);
         await addCollectionToUser(collectionReturn.user, collectionReturn.id);
         return collectionReturn;
+      } catch (e) {
+        error(e);
+        return e;
+      }
+    },
+    addStickerToCollection: async (_, { operationDetail }) => {
+      try {
+        const stickerDetailReturn = await addStickerToCollection(operationDetail);
+        return stickerDetailReturn;
       } catch (e) {
         error(e);
         return e;
