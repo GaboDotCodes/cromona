4e2848ae62163ddf3399bb3f7bb3a851b471cdb7
GaboDotCodes <gabedu2000@gmail.com>
2020-07-09 12:32:11 -0500

    Refactor server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const graphqlHTTP = require('express-graphql');
 const { makeExecutableSchema } = require('graphql-tools');
 const sentry = require('@sentry/node');
 const admin = require('firebase-admin');
-
+const typeDefs = require('./graphql/typesMerged');
+const resolvers = require('./graphql/resolversMerged');
 const { connect } = require('./mongodb/connect');
 
 const { PORT, DSN_SENTRY, ENV_SENTRY } = process.env;
@@ -12,13 +13,9 @@ const { log } = console;
 const app = express();
 
 sentry.init({ dsn: `${DSN_SENTRY}`, environment: `${ENV_SENTRY}` });
-connect();
-
 app.use(sentry.Handlers.requestHandler());
 
-const typeDefs = require('./graphql/typesMerged');
-
-const resolvers = require('./graphql/resolversMerged');
+connect();
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
