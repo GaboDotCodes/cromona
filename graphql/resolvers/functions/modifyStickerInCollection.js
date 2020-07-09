789289eb9de86b615a1c766c9f75b58d2ab9463d
GaboDotCodes <gabedu2000@gmail.com>
2020-07-09 18:55:11 -0500

    No nevative amounts

--- graphql/resolvers/functions/modifyStickerInCollection.js
+++ graphql/resolvers/functions/modifyStickerInCollection.js
@@ -2,8 +2,14 @@ const Collection = require('../../../mongodb/schemas/Collection');
 
 const modifyStickerInCollection = async (operationDetail, operation = 1) => {
   const { sticker, collection, user } = operationDetail;
+  const conditionGt = operation === 1 ? -1 : 0;
   const collectionUpdated = await Collection.findOneAndUpdate(
-    { _id: collection, user, 'stickersCollection.sticker': sticker },
+    {
+      _id: collection,
+      user,
+      'stickersCollection.amount': { $gt: conditionGt },
+      'stickersCollection.sticker': sticker,
+    },
     { $inc: { 'stickersCollection.$.amount': operation } },
     { new: true, useFindAndModify: true }
   ).populate('stickersCollection.sticker');
