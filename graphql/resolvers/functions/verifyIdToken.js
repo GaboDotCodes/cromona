ee3723a36f02cf71d6d6e8c63f6e6d567ce012f5
GaboDotCodes <gabedu2000@gmail.com>
2020-07-09 14:22:41 -0500

    Refactor verifyIdToken Function

--- graphql/resolvers/functions/verifyIdToken.js
+++ graphql/resolvers/functions/verifyIdToken.js
@@ -1,12 +1,7 @@
 const firebaseAdmin = require('firebase-admin');
 
-const { ADMINS_EMAILS } = process.env;
-
-const { emails } = JSON.parse(ADMINS_EMAILS);
-
 const verifyIdToken = async (authorization) => {
-  const { email, uid } = await firebaseAdmin.auth().verifyIdToken(authorization, true);
-  const admin = emails.includes(email);
-  return { uid, admin };
+  const decodedIdToken = await firebaseAdmin.auth().verifyIdToken(authorization, true);
+  return decodedIdToken;
 };
 module.exports = verifyIdToken;
