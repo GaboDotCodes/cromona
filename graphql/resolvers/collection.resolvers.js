d36e5904719d251761ea55ae466c390bda26135b
GaboDotCodes <gabedu2000@gmail.com>
2020-07-11 11:31:05 -0500

    getCollectionsByUserId Resolver

--- graphql/resolvers/collection.resolvers.js
+++ graphql/resolvers/collection.resolvers.js
@@ -6,6 +6,7 @@ const getAlbumById = require('./functions/getAlbumById');
 const getCollectionWithStickers = require('./functions/getCollectionWithStickers');
 const modifyStickerInCollection = require('./functions/modifyStickerInCollection');
 const getCollectionById = require('./functions/getCollectionById');
+const getCollectionsByUserId = require('./functions/getCollectionsByUserId');
 
 module.exports = {
   Mutation: {
@@ -51,6 +52,17 @@ module.exports = {
         await verifyMatchUserIdAndToken(queryData.user, authorization);
         const collectionReturn = await getCollectionById(queryData.collection);
         return collectionReturn;
+      } catch (e) {
+        error(e);
+        return e;
+      }
+    },
+    getCollectionsByUserId: async (_, { userId }, context) => {
+      try {
+        const { authorization } = context.headers;
+        await verifyMatchUserIdAndToken(userId, authorization);
+        const collectionsReturn = await getCollectionsByUserId(userId);
+        return collectionsReturn;
       } catch (e) {
         error(e);
         return e;
