ecad14d3b3fc9f040e06596500324444e3a85a5b
GaboDotCodes <gabedu2000@gmail.com>
2020-07-12 17:36:52 -0500

    The super function getPopularAlbums

---
+++ graphql/resolvers/functions/getPopularAlbums.js
@@ -0,0 +1,38 @@
+const User = require('../../../mongodb/schemas/User');
+
+const getPopularAlbums = async (location, ratio, unit) => {
+  const { lat, lon } = location;
+  let convertionToMt;
+  if (unit === 'mi') {
+    convertionToMt = 1609.34;
+  } else if (unit === 'km') {
+    convertionToMt = 1000;
+  } else {
+    throw new Error('getPopularAlbums: Invalid units');
+  }
+  const usersInRatio = await User.find({
+    lastLocation: {
+      $nearSphere: {
+        $geometry: {
+          type: 'Point',
+          coordinates: [lon, lat],
+        },
+        $maxDistance: ratio * convertionToMt,
+      },
+    },
+  }).populate({
+    path: 'collections',
+    populate: { path: 'album' },
+  });
+  const allAlbums = usersInRatio
+    .map((user) => {
+      return user.collections.map((collection) => {
+        return collection.album;
+      });
+    })
+    .flat();
+  const allAlbumsNoRepeat = [...new Set(allAlbums)];
+  return allAlbumsNoRepeat;
+};
+
+module.exports = getPopularAlbums;
