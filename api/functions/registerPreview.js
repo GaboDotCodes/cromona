4cc740a6eac02ec0d9128b72b347b163db19924e
GaboDotCodes <gabedu2000@gmail.com>
2020-10-26 11:35:59 -0500

    registerPreview Function

---
+++ api/functions/registerPreview.js
@@ -0,0 +1,17 @@
+const Preview = require('../../mongodb/schemas/Preview');
+
+const registerPreview = async (name, contact, referedBy) => {
+    if (referedBy) {
+        const registeredPreview = await Preview.findOne({ uid: referedBy });
+        if (!registeredPreview) throw 'No hay nadie registrado con esa referencia'
+        if ( registeredPreview.contact === contact ) throw 'No te puedes referir a ti mismo'
+    }
+
+    const registered = await Preview.findOne({ contact } )
+    if (registered) throw 'Ya estás registrado';
+
+    const registeredPreview = await Preview.create({ name, contact , referedBy });
+    return registeredPreview;
+};
+
+module.exports = registerPreview;
