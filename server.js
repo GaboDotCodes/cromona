65f839ba950c97e80ad23c652a7c344d6cfac098
GaboDotCodes <gabedu2000@gmail.com>
2020-10-27 11:07:39 -0500

    Cors

--- server.js
+++ server.js
@@ -23,6 +23,15 @@ app.use(sentry.Handlers.requestHandler());
 
 app.use(express.json());
 
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
 connect();
 
 admin.initializeApp({
