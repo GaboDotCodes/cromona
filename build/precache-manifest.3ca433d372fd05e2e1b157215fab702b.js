4a494ec725daf438490717e055b3d069a41548bc
GaboDotCodes <gabedu2000@gmail.com>
2020-11-02 07:25:25 -0500

    Build React App

---
+++ build/precache-manifest.3ca433d372fd05e2e1b157215fab702b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1fa63e872a3f45dd043e2c184e818c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef479c0ee03376a32a87",
+    "url": "/static/css/main.f922ba16.chunk.css"
+  },
+  {
+    "revision": "0430b82e46b577bd9c9e",
+    "url": "/static/js/2.a32a37cd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.a32a37cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef479c0ee03376a32a87",
+    "url": "/static/js/main.a35fdb78.chunk.js"
+  },
+  {
+    "revision": "ea523fb4a58f5d0faccb",
+    "url": "/static/js/runtime-main.ea4454ec.js"
+  },
+  {
+    "revision": "027d39b0e8b78feeab5c4017c53e5230",
+    "url": "/static/media/bg.027d39b0.jpg"
+  }
+]);
