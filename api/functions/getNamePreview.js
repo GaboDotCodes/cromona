3c730d7bf2c78face1c65853ebf86277181528e3
GaboDotCodes <gabedu2000@gmail.com>
2020-11-02 07:51:00 -0500

    Error

--- api/functions/getNamePreview.js
+++ api/functions/getNamePreview.js
@@ -1,7 +1,9 @@
 const Preview = require('../../mongodb/schemas/Preview');
 
 const getNamePreview = async (uid) => {
-    const { name } = await Preview.findOne({ uid });
+    const obj = await Preview.findOne({ uid });
+    if (!obj) throw 'Host no found'
+    const { name } = obj;
     const nameCap = name.split(" ")[0].charAt(0).toUpperCase() + name.split(" ")[0].slice(1)
     return nameCap;
 };
